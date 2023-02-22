689357c6806a27e4b578c2446031e490c015ff6d
Mily <mp_morones@hotmail.com>
2023-02-22 14:33:35 -0600

    Implement fetch api with useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+// Components
+import { Drawer, LinearProgress, Grid, Badge } from '@mui/material'
+import AddShoppingCartIcon from '@mui/material/Icon'
+// import Badge from '@mui/material/Badge'
+// Styles
+import { Wrapper } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
 
-function App() {
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+}
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
